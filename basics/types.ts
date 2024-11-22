ee7660e3be5bd5449dd5a810ca2a1e4f2bd79796
Paula Alvarez <alvarez.paula.maria@gmail.com>
2024-11-22 19:24:57 -0300

    Tipos de datos

--- basics/types.ts
+++ basics/types.ts
@@ -7,4 +7,25 @@ let fruits: Array<string> = [];
 let cats: string[] = [];
 
 let response: any = "";
+let response2: unknown;
+let response3: null;
+let response4 : undefined;
 
+function saludas(): void{
+    console.log("Hola")
+}
+
+// Hay situaciones en donde nuestra variable puede ser de varios tipos
+
+let response5: number | null = 5;
+response5?.toString();
+
+// Podemos crear nuestros propios tipos de datos, por ejemplo:
+
+// let responseProductos : number | null;
+// let responsePartners: number | null;
+
+type ResponseTypeService = number | null;
+
+let responseProductos : ResponseTypeService;
+let responsePartners: ResponseTypeService;
---
+++ dist/types.js
@@ -0,0 +1,18 @@
+var age = 20;
+var myName = "Paula";
+var isSemiSenior = true;
+var person = {};
+var fruits = [];
+var cats = [];
+var response = "";
+var response2;
+var response3;
+var response4;
+function saludas() {
+    console.log("Hola");
+}
+// Hay situaciones en donde nuestra variable puede ser de varios tipos
+var response5 = 5;
+response5 === null || response5 === void 0 ? void 0 : response5.toString();
+var responseProductos;
+var responsePartners;

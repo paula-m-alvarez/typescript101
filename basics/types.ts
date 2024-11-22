57f24265601f9a434067a4fa508c3941d50d06ec
Paula Alvarez <alvarez.paula.maria@gmail.com>
2024-11-22 19:12:53 -0300

    Tipos de datos

---
+++ basics/types.ts
@@ -0,0 +1,10 @@
+let age: number = 20;
+let myName: string = "Paula";
+let isSemiSenior: boolean = true;
+let person: Object = {};
+
+let fruits: Array<string> = [];
+let cats: string[] = [];
+
+let response: any = "";
+

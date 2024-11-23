8686814f9cbbd7eacb99a298d507979c2cfbc769
Paula Alvarez <alvarez.paula.maria@gmail.com>
2024-11-23 14:12:03 -0300

    Functions

---
+++ basics/functions.ts
@@ -0,0 +1,17 @@
+const sayHi = (name) => {
+    console.log(`Hola ${name}`);
+}
+
+sayHi("estudiantes");
+
+function sayGoodbye(name:string) {
+    console.log("Adios")
+}
+
+sayGoodbye("Test");
+
+function show() : number{
+    return 2;
+}
+
+// Tipamos la funcion verificando el tipo de valor que tiene que devolver y sus parametros
